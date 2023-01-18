cfa9ed046739d30649be4b0324f2eda4804cd82f
rklf <11515950+rklf@users.noreply.github.com>
2023-01-18 19:17:18 +0100

    add homeowner home and tenant home

---
+++ app/src/components/Layout.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <v-main>
+        <v-container fluid>
+            <slot />
+        </v-container>
+    </v-main>
+</template>
+
+<style scoped lang="scss">
+.v-main {
+    display: flex;
+    max-height: 100vh;
+
+    .v-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        height: 100%;
+    }
+}
+</style>
---
+++ app/src/views/HomeowerHome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import Layout from '@/components/Layout.vue';
+</script>
+
+<template>
+    <Layout>
+        <template v-slot>
+            <Navbar for="homeowner" />
+            <Hero for="homeowner" />
+        </template>
+    </Layout>
+</template>
+
+<style scoped lang="scss">
+
+</style>
