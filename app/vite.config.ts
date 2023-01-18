e5db49032509d075584680d09a181675149f9061
Florian Perrier <fperrier@wedigital.garden>
2023-01-18 11:06:02 +0100

    add vuetify

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,14 +2,26 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 4000,
+    hmr: {
+      clientPort: 4000,
+    },
+    watch: {
+      usePolling: true,
+    },
   },
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    vuetify({ autoImport: true }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./app/src", import.meta.url)),
