fc931505001f83d8e248ecf5f42e16f396713576
Wendy Afrim <wendy.afrim2@gmail.com>
2023-01-18 16:32:47 +0100

    Integrate a form to register an owner Call POST to save in database done

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TenantHome from '@/views/TenantHome.vue';
 import HomeownerHome from '@/views/HomeowerHome.vue';
+import OwnerView from '../views/OwnerView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,10 +12,15 @@ const router = createRouter({
       component: TenantHome,
     },
     {
-      path: '/proprietaire',
+      path: '/homeowner',
       name: 'homeowner_home',
       component: HomeownerHome,
     },
+    {
+      path: '/homeowner/register',
+      name: 'homeowner_register',
+      component: OwnerView,
+    }
   ]
 })
 
---
+++ app/src/views/OwnerView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Owner from '../components/Owner.vue'
+</script>
+
+<template>
+  <main>
+    <Owner />
+  </main>
+</template>
